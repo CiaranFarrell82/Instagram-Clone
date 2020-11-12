ecd532cacd651d5c5677197a1218f010e7a67ce3
CiaranFarrell82 <ciaran.farrell82@mail.dcu.ie>
2020-11-12 12:54:27 +0000

    adding login ui

---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Ciaran Farrell on 11/11/2020.
+//  Copyright © 2020 Ciaran Farrell. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
